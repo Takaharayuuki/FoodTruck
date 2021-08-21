bba3a2e9d5b5eaea5892c21f749f3774a68ab3ef
yuuki_takahara <takahara.yuuki22@gmail.com>
2021-08-21 11:39:30 +0900

    frontend 環境構築

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/{any}', function () {
+    logger('welcome route.');
+    return view('spa.app');
+})->where('any', '.*');
---
+++ frontend/vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  // アセットはLaravelのpublic/appディレクトリ配下に作成される
+  outputDir: "../backend/public/app",
+
+  // app配下にjs, cssなどが置かれるので、publicPathを調整
+  publicPath: "/app",
+
+  pages: {
+    // appのエントリポイント、テンプレート、出力先を調整
+    app: {
+      entry: "src/main.ts",
+      template: "templates/base.html",
+      filename: `../../resources/views/spa/app.blade.php`,
+    },
+  },
+};
