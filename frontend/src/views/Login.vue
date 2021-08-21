aa8029048d79d9e0ed6c70ddbef46e6d6e68fcd4
yuuki_takahara <takahara.yuuki22@gmail.com>
2021-08-21 15:15:34 +0900

    Tailwind css セットアップ #1

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1 class="text-4xl text-green-700 text-center font-semibold">
+      Hello Tailwind
+    </h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Login",
+});
+</script>
