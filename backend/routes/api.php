f9b4351d8ff09a4a152a386d6729f932a0462bda
yuuki_takahara <takahara.yuuki22@gmail.com>
2021-10-09 10:51:42 +0900

    Review Controllerの作成 #26

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,26 +6,13 @@ use APP\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\StoreController;
 use App\Http\Controllers\ProductController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 // 会員登録
 Route::post('/register', [App\Http\Controllers\Auth\RegisterController::class, 'register']);
-
 // ログイン / ログアウト
 Route::prefix('auth')->group(function () {
     Route::post('/login', [App\Http\Controllers\Auth\LoginController::class, 'login']);
     Route::post('/logout', [App\Http\Controllers\Auth\LoginController::class, 'logout']);
 });
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
@@ -35,3 +22,5 @@ Route::resource('stores', StoreController::class);
 Route::post('stores/search', [App\Http\Controllers\StoreController::class, 'search'])->name('stores.search');
 // 商品関連
 Route::resource('products', ProductController::class);
+// クチコミ関連
+Route::post('reviews/store', [App\Http\Controllers\ReviewController::class, 'store'])->name('reviews.store');
